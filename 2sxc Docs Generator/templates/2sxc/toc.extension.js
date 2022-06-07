becc60fb9ebf6b25acaa07f13a349d7ab7ffc4de
ijungleboy <daniel.mettler@2sic.com>
2022-06-07 16:51:46 +0200

    extract / refactor toc-generation

--- 2sxc Docs Generator/templates/2sxc/toc.extension.js
+++ 2sxc Docs Generator/templates/2sxc/toc.extension.js
@@ -5,24 +5,19 @@
  */
 const ns = require('./api-meta.js');
 const dbg = require('./toc-debug.js');
-
-// Experimental - get Object.assign to work using Polyfill
-const polyfills = require('./polyfill-object.assign.js');
+const toc = require('./toc-tools.js');
+toc.namespacePrefixes = ns.prefixes;
+toc.truncEllipsis = "…";
+toc.nodeDefaults = ns.defaultSettings;
+toc.nodeData = ns.data;
 
 // Constants etc.
 const tocLevelTop = 1;
-
-const nsKeepParts = 3;    // Namespace parts to keep, like ToSic.Eav.ImportExport
-const nsTruncateToParts = 2;   // If NS has more parts, keep only the last two (and prefix with ...)
 
 // Debug Parameters
 const dbgProcessNode = false;
 const dbgProcessNodeJustAFewMax = 0;
 const dbgSortNetToc = false;
-const dbgIsApiToc = false;
-let count = 0;
-
-
 
 /**
  * This method will be called at the start of exports.transform in toc.html.js
@@ -36,15 +31,15 @@ exports.preTransform = function (model) {
  * This method will be called at the end of exports.transform in toc.html.js
  */
 exports.postTransform = function (model) {
-  const isApi = isApiToc(model);
+  const isApi = toc.isTopLevelApiToc(model);
   if (isApi) {
     dbg.log('postTransform start as isApiToc for ', model, 25);
     processNode(model, tocLevelTop);
+    // Only sort the items if we are really on the top-level of our namespace
+    model.items = sortNetToc(model);
   } else {
     dbg.log('postTransform skip as !isApiToc for ', model, 25);
   }
-  // dbg.log("count:" + count);
-  // dbg.log(logPrefix + 'postTransform end');
   return model;
 }
 
@@ -52,42 +47,6 @@ exports.postTransform = function (model) {
 
 // ----------------------------------------------------------------------------------------------------
 
-
-// find out if it's the API toc
-function isApiToc(model) {
-  if(!model) return false;
-
-  // find out if it's the TOC of the API
-  if(!(model.items && model.items.length))
-    return false;
-
-  var firstName = model.items[0].topicUid || model.items[0].name;
-  var match = isNamespace(firstName);
-
-  // Debug
-  if (dbgIsApiToc)
-    if (firstName && firstName.indexOf(ns.prefixes[0] /*+ ".Dnn" */) === 0)
-      dbg.warn('isApiToc Dnn:', model);
-
-  return match;
-}
-
-// check if a string is likely a namespace API prefix
-function isNamespace(name) {
-  if (!name) return false;
-  for (let i = 0; i < ns.prefixes.length; i++)
-    if (name.indexOf(ns.prefixes[i]) === 0) return true;
-  return false;
-}
-
-// repeat a string X times
-function repeatString(part, count) {
-  if(count <= 0) return "";
-  var result = "";
-  for(i = 0; i < count; i++)
-    result += part;
-  return result;
-}
 
 let dbgProcessNodeJustAFew = 0;
 function processNode(item, level) {
@@ -102,17 +61,12 @@ function processNode(item, level) {
     dbg.log('debug processNode[' + level + "] ", item);
   }
 
-  const isOurNamespace = isNamespace(item.topicUid || item.name);
-  if (isOurNamespace) {
-    // add metadata - before changing the namespace
-    addMeta(item, level);
-    if(level <= 2)
-      shortenNamespace(item, level);
-  }
-  else
-  {
-    addMeta(item, level, false);
-  }
+  // add metadata - before changing the namespace
+  toc.addNodeData(item, level);
+
+  const isOurNamespace = toc.isNamespace(item.topicUid || item.name);
+  if (isOurNamespace && level <= 2)
+    toc.shortenNamespace(item);
 
   item.level = level;
   if (item.items && item.items.length > 0) {
@@ -120,10 +74,6 @@ function processNode(item, level) {
     for (var i = 0; i < length; i++) {
       processNode(item.items[i], level + 1);
     }
-
-    // Only sort the items if we are really on the top-level of our namespace
-    if (level === tocLevelTop)
-      item.items = sortNetToc(item);
   }
 }
 
@@ -134,61 +84,10 @@ function sortNetToc(item) {
   if (dbgSortNetToc) dbg.error('top', top);
   const rest = item.items.filter(function(i) { return !i || i.top !== true; });
   if (dbgSortNetToc) dbg.error('rest', rest);
-  const all = [manualTocEntry("<strong>Top Namespaces</strong>")]
+  const all = [toc.createLeaf("<strong>Top Namespaces</strong>")]
     .concat(top)
-    .concat([manualTocEntry("<hr>")])
+    .concat([toc.createLeaf("<hr>")])
     .concat(rest);
   if (dbgSortNetToc) dbg.error('all', all);
   return all;
-}
-
-function manualTocEntry(name) {
-  return {
-    "name": name,
-    // "href": "Custom.Dnn.html",
-    // "topicHref": "Custom.Dnn.html",
-    // "topicUid": "Custom.Dnn",
-    "tocHref": null,
-    "level": 2,
-    "items": [],
-    "leaf": true,
-    "priority": "", // "custom",
-    // "fullName": "Custom.Dnn"
-  }  
-}
-
-/**
- * shorten ... the namespace
- */
-function shortenNamespace(item, level) {
-  item.fullName = item.name;
-  var parts = item.name.split('.');
-  var count = parts.length;
-  if (count > nsKeepParts) {
-    parts.splice(0, count - nsTruncateToParts);
-    var newName = repeatString("...", count - nsKeepParts) + parts.join('.');
-    item.name = newName;
-  }
-}
-
-
-
-/**
- * add metadata for the template to prioritizes
- * @param {*} item 
- * @param {*} level 
- */
-function addMeta(item, level, debug) {
-  setPriorityNormal(item);
-
-  var found = ns.data[item.topicUid];
-  if (found) {
-    if (debug) dbg.warn('JS Debug addMeta - uid:' + item.topicUid);
-    polyfills.objectAssign(item, found);
-  }
-}
-
-function setPriorityNormal(item) {
-  count++;
-  polyfills.objectAssign(item, ns.defaultSettings);
 }
