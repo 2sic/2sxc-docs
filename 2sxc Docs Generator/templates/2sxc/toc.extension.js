c2fa0d1ef132a9da7695b7c3279131b9c96e3ed1
ijungleboy <daniel.mettler@2sic.com>
2023-08-17 14:19:35 +0200

    improve TOC and fix some linking issues

--- 2sxc Docs Generator/templates/2sxc/toc.extension.js
+++ 2sxc Docs Generator/templates/2sxc/toc.extension.js
@@ -34,24 +34,82 @@ exports.postTransform = function (model) {
   const isApi = toc.isTopLevelApiToc(model);
   if (isApi) {
     dbg.log('postTransform start as isApiToc for ', model, 25);
-    processNode(model, tocLevelTop);
+    processNodeRecursive(model, tocLevelTop, changeNodeNetApi);
     // Only sort the items if we are really on the top-level of our namespace
     model.items = sortNetApiToc(model);
   } else {
     dbg.log('postTransform skip as !isApiToc for ', model, 25);
+    processNodeRecursive(model, tocLevelTop, changeNodeAddLines);
   }
   return model;
 }
 
+/**
+ * apply changes such as converting '---' to <hr>
+ * and ensure that they will be "empty" nodes without link tags
+ * this is important because the empty <a> tags caused navigation issues in the browser
+ * @param {*} node 
+ * @param {*} level 
+ * @returns 
+ */
+function changeNodeAddLines(node, level) {
+  if (!node || !node.name) return;
 
+  // Parse --- and <hr> tags
+  if (node.name == '---' || node.name == '<hr>') {
+    node.name = '<hr>';
+    unlinkNode(node);
+    // dbg.log('changeNodeAddLines - found ' + node.name, node);
+    // dbg.log(JSON.stringify(node));
+    return;
+  }
+
+  // parse tags like **Some Title**
+  if (node.name.indexOf('**') == 0 && (node.name.lastIndexOf('**') == node.name.length - 2)) {
+    // dbg.log('index of **' + node.name.indexOf('**'));
+    // dbg.log('last index of **' + node.name.lastIndexOf('**'));
+    // dbg.log('length ' + node.name.length);
+    // dbg.log('last index of **' + (node.name.lastIndexOf('**') == node.name.length - 2));
+    // dbg.log('has nodes ' + (node.items && node.items.length > 0));
+    const namePart = node.name.replaceAll('**', '');
+    // dbg.log('final name: ' + namePart);
+    node.name = `<strong>${namePart}</strong>`;
+    unlinkNode(node);
+    node.topicHref = '#test';
+    return;
+  }
+
+  // parse an explicit test title, which is usually not available
+  // but you can add one any time to test other logic
+  if (node.name.indexOf('TestXXX') > -1) {
+    dbg.log('changeNodeAddLines - found ' + node.name, node);
+    dbg.log(JSON.stringify(node));
+    unlinkNode(node);
+    return;
+  }
+}
+
+function unlinkNode(node) {
+  node.topicUid = null;
+  node.topicHref = null;
+  // custom property we add to ensure the li-template doesn't add a link
+  node.noLink = true;
+}
 
 // ----------------------------------------------------------------------------------------------------
 
-
+// Debug Counter to only debug a few items
 let dbgProcessNodeJustAFew = 0;
-function processNode(item, level) {
+
+/**
+ * 
+ * @param {*} node the current node
+ * @param {*} level the current level, as different levels may change the behavior
+ * @param {*} modifyNodeCall the callback to modify each node
+ */
+function processNodeRecursive(node, level, modifyNodeCall) {
   if (dbgProcessNodeNetApi && dbgProcessNodeJustAFew < dbgProcessNodeJustAFewMax) {
-    dbg.log('processNode item: [lvl:' + level + ']:', item);
+    dbg.log('processNode item: [lvl:' + level + ']:', node);
     dbgProcessNodeJustAFew++;
     if (dbgProcessNodeJustAFew < dbgProcessNodeJustAFewMax)
       dbg.log(`will stop logging activity as we've reached ${dbgProcessNodeJustAFewMax}`)
@@ -59,24 +117,33 @@ function processNode(item, level) {
 
   // debug data on item
   // var debugModel = JSON.stringify(item);
-  if (dbgProcessNodeNetApi && item.topicUid && item.topicUid.indexOf("Custom") > -1) {
-    dbg.log('debug processNode[' + level + "] ", item);
+  if (dbgProcessNodeNetApi && node.topicUid && node.topicUid.indexOf("Custom") > -1) {
+    dbg.log('debug processNode[' + level + "] ", node);
   }
 
-  // add metadata - before changing the namespace
-  toc.addNodeData(item, level);
+  // Modify the node using the passed in function
+  modifyNodeCall(node, level);
 
-  const isOurNamespace = toc.isNamespace(item.topicUid || item.name);
-  if (isOurNamespace && level <= 2)
-    toc.shortenNamespace(item);
+  // Add level information to the current node
+  node.level = level;
 
-  item.level = level;
-  if (item.items && item.items.length > 0) {
-    var length = item.items.length;
+  // Traverse the tree
+  if (node.items && node.items.length > 0) {
+    var length = node.items.length;
     for (var i = 0; i < length; i++) {
-      processNode(item.items[i], level + 1);
+      processNodeRecursive(node.items[i], level + 1, modifyNodeCall);
     }
   }
+}
+
+function changeNodeNetApi(node, level) {
+  // dbg.log('changeNodeNetApi', node, level);
+  // add metadata - before changing the namespace
+  toc.addNodeData(node, level);
+
+  const isOurNamespace = toc.isNamespace(node.topicUid || node.name);
+  if (isOurNamespace && level <= 2)
+    toc.shortenNamespace(node);  
 }
 
 function sortNetApiToc(item) {
