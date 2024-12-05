46a632313e28aadf209dd8876cb95b48a865fef1
iJungleboy <daniel.mettler@2sic.com>
2024-12-05 16:14:00 +0100

    correct current version in version switcher

--- 2sxc Docs Generator/templates/2sxc/src/versions.ts
+++ 2sxc Docs Generator/templates/2sxc/src/versions.ts
@@ -1,6 +1,6 @@
 export function setVersionButtonLink() {
   const versionSelector = document.getElementsByClassName('version-button')[0] as HTMLAnchorElement;
   var oldLink = versionSelector.href;
-  var newLink = oldLink + '?version=17.00&path=' + window.location.pathname;
+  var newLink = oldLink + '?version=18.00&path=' + window.location.pathname;
   versionSelector.setAttribute('href', newLink);
 }
