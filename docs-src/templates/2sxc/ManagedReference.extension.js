fc7f9d20184bcc0128002d0f666ef0eb7081f616
iJungleboy <daniel.mettler@2sic.com>
2026-02-19 23:52:47 +0100

    introduce code-warning for WIP attributes

--- docs-src/templates/2sxc/ManagedReference.extension.js
+++ docs-src/templates/2sxc/ManagedReference.extension.js
@@ -23,28 +23,39 @@ const internalNamespaces = [
   /^ToSic\..*.(Sys|Internal)(\..*|$)/,
 ];
 
+const DefInternalWarningSuffix = `is an internal API and can change at any time. 
+  It's documented so you can understand how it works. <br>
+  ⚠️ Do not use it, or anything with <code>Sys</code> or <code>Internal</code> namespaces in your code, as future changes could break your code.`;
+
+const DefWipWarningSuffix = `is a work-in-progress API and can change at any time. 
+  It's documented so you can understand how it works. <br>
+  ⚠️ Do not use it, or anything with <code>Sys</code> or <code>Internal</code> namespaces in your code, as future changes could break your code.`;
+
 /**
  * This method will be called at the end of exports.transform in ManagedReference.html.primary.js
  */
 exports.postTransform = function (model) {
-
-  const DefMessageSuffix = `is an internal API and can change at any time. 
-  It's documented so you can understand how it works. <br>
-  ⚠️ Do not use it, or anything with <code>Sys</code> or <code>Internal</code> namespaces in your code, as future changes could then break your code.`;
 
   /* Internal API warning for the ToSic.Lib namespace */
   // If the model UID starts with "ToSic.Lib", treat it as internal
   const hasInternalNamespace = tools.modelNamespaceStartsWithAny(model, internalNamespaces)
     || tools.modelNamespaceContainsAny(model, [".Internal."]);
   if (hasInternalNamespace) {
-    tools.addAlert(model, `ℹ️ This ${model.type.toLowerCase()} ${DefMessageSuffix}`, "warning");
+    tools.addAlert(model, `ℹ️ This ${model.type.toLowerCase()} ${DefInternalWarningSuffix}`, "warning");
   }
 
   /* Internal API warning for internal classes or interfaces based on attributes */
   // Check if the model has attributes indicating it is internal
   if (tools.modelIsDecoratedWith(model, "InternalApi")) {
     // Display a warning with the specific model type (e.g., "This class is...", "This interface is...")
-    tools.addAlert(model, `ℹ️ This ${model.type.toLowerCase()} ${DefMessageSuffix}`, "warning");
+    tools.addAlert(model, `ℹ️ This ${model.type.toLowerCase()} ${DefInternalWarningSuffix}`, "warning");
+  }
+
+  /* Work-in-progress API warning for classes or interfaces based on attributes */
+  // Check if the model has attributes indicating it is a work-in-progress API
+  if (tools.modelIsDecoratedWith(model, "WorkInProgressApi")) {
+    // Display a warning with the specific model type (e.g., "This class is...", "This interface is...")
+    tools.addAlert(model, `ℹ️ This ${model.type.toLowerCase()} ${DefWipWarningSuffix}`, "danger");
   }
 
   // If internal methods exist, inject warning alerts into each one
@@ -52,22 +63,54 @@ exports.postTransform = function (model) {
     // Inject inline alert into each affected method
     model.children?.forEach((child) => {
       // Check if the child has nested children
-      if (Array.isArray(child.children)) {
-        child.children.forEach((method) => {
-          // Check if the method has any InternalApi attribute
-          const isInternal = method.attributes?.some(
-            (attr) => attr.type.includes("InternalApi")
-          );
+      if (!Array.isArray(child.children))
+        return;
+      
+      child.children.forEach((method) => {
+        // Check if the method has any InternalApi attribute
+        const isInternal = method.attributes?.some(
+          (attr) => attr.type.includes("InternalApi")
+        );
 
-          // If internal, prepend the alert to the summary content
-          if (isInternal) {
-            // Overwrite the summary with an alert message and add the original summary at the end
-            method.summary = tools.createHtmlAlert(`⚠️ This ${DefMessageSuffix}`, "warning") + (method.summary || "");
-          }
-        });
-      }
+        // If internal, prepend the alert to the summary content
+        if (isInternal) {
+          // Overwrite the summary with an alert message and add the original summary at the end
+          method.summary = tools.createHtmlAlert(`⚠️ This ${DefInternalWarningSuffix}`, "warning") + (method.summary || "");
+        }
+
+      });
     });
   }
 
+  // Custom test to debug a specific namespace and model - can be removed later
+  // if (model.uid == 'Custom.Hybrid.RazorTyped') {
+  //   var debugObj = model.children[0].children[3];
+  //   var has = tools.hasMemberWithAttribute(model, "WorkInProgressApi");
+  //   console.log("Debug: Model UID is Custom.Hybrid.RazorTyped, model: " + has + ";" + JSON.stringify(debugObj));
+  // }
+
+  // If work-in-progress methods exist, inject warning alerts into each one
+  if (tools.hasMemberWithAttribute(model, "WorkInProgressApi")) {
+    // console.warn("⚠️ Work-in-progress API method found in model: " + model.uid);
+    // Inject inline alert into each affected method
+    model.children?.forEach((child) => {
+      // Check if the child has nested children
+      if (!Array.isArray(child.children))
+        return;
+      
+      child.children.forEach((method) => {
+        // Check if the method has any WorkInProgressApi attribute
+        const isWip = method.attributes?.some(
+          (attr) => attr.type.includes("WorkInProgressApi")
+        );
+
+        // If work-in-progress, prepend the alert to the summary content
+        if (isWip) {
+          // Overwrite the summary with an alert message and add the original summary at the end
+          method.summary = tools.createHtmlAlert(`⚠️ This ${DefWipWarningSuffix}`, "danger") + (method.summary || "");
+        }
+      });
+    });
+  }
   return model;
 };
